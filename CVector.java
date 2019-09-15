8b42a062daff49e5af7a3085878d61656340a3ef
Metaraddin <48632825+Metaraddin@users.noreply.github.com>
2019-09-15 10:58:35 +0300

    Правки

--- CVector.java
+++ CVector.java
@@ -1,21 +1,33 @@
 package com.company;
 
 public class CVector {
-    private double _x;
-    private double _y;
-    private double _z;
+    private double mX;
+    private double mY;
+    private double mZ;
 
     public CVector(double x, double y, double z) {
-        _x = x;
-        _y = y;
-        _z = z;
+        mX = x;
+        mY = y;
+        mZ = z;
     }
 
-    public double len () {
-        return Math.sqrt(_x*_x + _y*_y + _z*_z);
+    public CVector() {
+        mX = 0;
+        mY = 0;
+        mZ = 0;
+    }
+
+    public double len() {
+        return Math.sqrt(mX*mX + mY*mY + mZ*mZ);
     }
 
     public double scalP(double x, double y, double z) {
-        return (_x*x + _y*y + _z*z);
+        return (mX*x + mY*y + mZ*z);
+    }
+
+    public String print() {
+        return ("X: " + Double.toString(mX) +
+                "\nY: " + Double.toString(mY) +
+                "\nZ: " + Double.toString(mZ));
     }
 }
--- Main.java
+++ Main.java
@@ -1,5 +1,17 @@
+package com.company;
+
 public class Main {
-    public static void main(String[] args){
-        System.out.println("Hello, Java!");
+
+    public static void main(String[] args) {
+        System.out.println("Hello, Java!\n");
+
+        CVector vec1 = new CVector(23,11,4);
+        CVector vec2 = new CVector();
+
+        System.out.println(vec1.len());
+        System.out.println(vec1.scalP(1,12,6) + "\n");
+
+        System.out.println(vec1.print() + "\n");
+        System.out.println(vec2.print());
     }
 }
