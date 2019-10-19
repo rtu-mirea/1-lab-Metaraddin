83835d82768124a64da0bcfca49cddd5550c5e20
Metaraddin <48632825+Metaraddin@users.noreply.github.com>
2019-10-19 10:01:34 +0300

    upd

--- Lab3/CMenu.java
+++ Lab3/CMenu.java
@@ -13,37 +13,70 @@ public class CMenu {
     public CMenu() {
         songs = new CSongsList();
         users = new CUserList();
+        users.registration("Admin", "admin", "admin", true);
     };
 
-    public void enter() {
+    public void startMenu() {
         Scanner scan = new Scanner(System.in);
+        int task = -1;
+        while (task != 0) {
+            System.out.println("1 - Sing Up\n2 - Sign In\n0 - Exit");
+            task = scan.nextInt();
+            switch (task) {
+                case(1):
+                    registration();
+                    break;
+                case(2):
+                    enter();
+                    break;
+                case(0):
+                    break;
+            }
+        }
+    }
+
+    private void registration() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Name: ");
+        String name = scan.next();
         System.out.print("Login: ");
         String login = scan.next();
+        System.out.print("Password: ");
+        String password = scan.next();
+        users.registration(name, login, password, false);
+        startMenu();
+    }
+
+    private void enter() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Login: ");
+        String login = scan.next().trim();
         System.out.print("Password: ");
         String password = scan.next();
         int user = users.enter(login,password);
         if (user != -1) {
             current = users.getUser(user);
             System.out.println("Welcome, " + current.getName() + "!");
+            showMainMenu();
         } else {
             System.out.println("Authorization Failed");
+            startMenu();
         }
-        showMainMenu();
     }
 
     private void showAdminMenu() {
-        System.out.println("1 - End day\n2 - Change user\n0 - Exit");
         int task = -1;
+        Scanner scan = new Scanner(System.in);
         while (task != 0) {
-            Scanner scan = new Scanner(System.in);
+            System.out.println("1 - End day\n2 - Change user\n0 - Exit");
             task = scan.nextInt();
             switch (task) {
                 case(1):
                     System.out.print("List lenght: ");
-                    songs.result(scan.nextInt());
+                    System.out.println(songs.result(scan.nextInt()));
                     break;
                 case(2):
-                    enter();
+                    startMenu();
                     break;
                 case(0):
                     System.out.println("Bye-Bye!");
@@ -53,25 +86,21 @@ public class CMenu {
     }
 
     private void showFanMenu() {
-        System.out.println("1 - Suggest a song\n\n2 - View all songs\n3 - Change user\n0 - Exit");
         int task = -1;
+        Scanner scan = new Scanner(System.in);
         while (task != 0) {
-            Scanner scan = new Scanner(System.in);
+            System.out.println("1 - Suggest a song\n2 - View all songs\n3 - Change user\n0 - Exit");
             task = scan.nextInt();
             switch (task) {
                 case(1):
                     System.out.print("Enter song title: ");
-                    if (songs.vote(current, scan.next())) {
-                        System.out.println("You voted.");
-                    } else {
-                        System.out.println("Your vote was first.");
-                    }
+                    System.out.println(songs.vote(current, scan.next()));
                     break;
                 case(2):
                     System.out.println(songs.print());
                     break;
                 case(3):
-                    enter();
+                    startMenu();
                     break;
                 case(0):
                     System.out.println("Bye-Bye!");
--- Lab3/CSongsList.java
+++ Lab3/CSongsList.java
@@ -12,34 +12,43 @@ public class CSongsList {
     public String print() {
         String res = "";
         for (int i = 0; i < songs.size(); i++) {
-            res += i + ") " + songs.get(i).getName();
+            res += i + ") " + songs.get(i).getName() + " (" + songs.get(i).getVote() + ")\n";
         }
         return res;
     }
 
-    public boolean vote(CUser inp, String song) {
+    public String vote(CUser inp, String song) {
         for (int i = 0; i < songs.size(); i++) {
-            if (songs.get(i).getName() == song) {
-                songs.get(i).toVote(inp);
-                return true;
+            if (songs.get(i).getName().equals(song)) {
+                if (songs.get(i).checkUser(inp)) {
+                    songs.get(i).toVote(inp);
+                    return "You voted.";
+                }
+                else {
+                    return "You have already voted.";
+                }
             }
         }
         songs.add(new CSong(song));
-        songs.get(songs.size()).toVote(inp);
-        return false;
+        songs.get(songs.size() - 1).toVote(inp);
+        return "Your vote was first.";
     }
 
     public String result(int len) {
+        if (len > songs.size()) { len = songs.size(); }
         String res = "";
         int j = 1;
-        while (!songs.isEmpty() | j <= len) {
+        while (j <= len) {
+            //System.out.println(!songs.isEmpty());
             int reference = 0;
             for (int i = 0; i < songs.size(); i++) {
                 if (songs.get(i).getVote() > songs.get(reference).getVote()) {
                     reference = i;
                 }
             }
+            //System.out.println(reference);
             res += j + ") " + songs.get(reference).getName() + "\n";
+            j++;
             songs.remove(reference);
         }
         return res;
